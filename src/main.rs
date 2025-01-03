d7151e313e1debb60795f532a2a5db9cdd7a9ab3
Anderson Acuña <andersonyhavs@outlook.com>
2025-01-02 21:39:26 -0500

    Add some types of IP address to V4 variant.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
-enum IpVersions {
-    V4,
+#[derive(Debug)]
+enum IpAddress {
+    V4 { address: String, ip_type: IpTypes },
     V6,
 }
 
+#[derive(Debug)]
+enum IpTypes {
+    Loopback,
+    Private,
+    Public,
+    Broadcast,
+}
+
+impl IpAddress {
+    fn whoami(&self) {
+        println!("I'm {:?}", self);
+    }
+}
+
 fn main() {
-    let ipv4 = IpVersions::V4;
+    let myhouse = IpAddress::V4 {
+        address: String::from("221.211.103.093"),
+        ip_type: IpTypes::Public,
+    };
     println!("Examples of enums running.");
+    myhouse.whoami();
 }
